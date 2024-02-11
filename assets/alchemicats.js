a3297bf6c4561478a6778748f97163df7fdeee34
Dekram McKraken <mkythethinker@mky-soft.com>
2024-02-11 15:24:50 +0100

    pager

--- assets/alchemicats.js
+++ assets/alchemicats.js
@@ -1,23 +1,54 @@
 
 class Alchemicats {
     constructor() {
+
+        this.pages = [];
+
         document.querySelectorAll('nav[data-page="swiper"]').forEach(swiper => {
             swiper.addEventListener("click", async (evt) => {
                 evt.preventDefault();
-                this.swipe(evt.target.dataset.page, evt.target.dataset.pageIndex);
+                this.swipe(evt.target.dataset.page,
+                    evt.target.dataset.pageIndex,
+                    evt.target.dataset.pageDescription);
             });
         });
-    }
 
+       
+    }
+    defaultOrCachePages = async () => {
+        let cache = sessionStorage.getItem("_cache");
+
+        if (cache == null) {
+            this.pages = [];
+            document.querySelectorAll(`article[data-page]`).forEach(async section => {
+                let page = {
+                    name: section.dataset.page,
+                    index: 1,
+                    description: section.dataset.pageDescription
+                };
+                this.pages.push(page);
+            });
+        }
+        else {
+            this.pages = JSON.parse(cache);   
+        }
+        sessionStorage.setItem("_cache", JSON.stringify(this.pages));
+    }
     get = async (name) => {
         const response = await fetch(`leaf/${name}.html`);
         const page = await response.text();
         return page;
     }
-    swipe = async (name, index) => {
-        document.querySelectorAll(`section[data-page=${name}]`).forEach(async section => {
+    swipe = async (name, index, description) => {
 
-            //const html = await this.get("test");
+        var currentPage = this.pages.find((page) => {
+            return page.name == name;
+        });
+
+        if (currentPage) {
+            currentPage.index = index;
+        }
+        document.querySelectorAll(`section[data-page=${name}]`).forEach(async section => {
 
             if (parseInt(section.dataset.pageIndex) != index) {
                 section.classList.add("hidden");
@@ -25,32 +56,35 @@ class Alchemicats {
             else {
                 section.classList.remove("hidden");
             }
-            sessionStorage.setItem(name, index);
+
             document.querySelectorAll(`a[data-page="${name}"]`).forEach(nav => {
                 nav.classList.remove('active');
             });
             document.querySelectorAll(`a[data-page="${name}"][data-page-index="${index}"]`).forEach(nav => {
                 nav.classList.add('active');
             });
+            document.querySelector(`#${name} h2`).innerHTML = `${description} (${index})`;
         });
+
+        sessionStorage.setItem("_cache", JSON.stringify(this.pages));
     };
-    load = async () => {
-       
+    init = async () => {
         this.find("main header").innerHTML = await this.get("header");
         this.find("#socialMedia").innerHTML = await this.get("socialMedia");
+        this.pages = await this.defaultOrCachePages();
+        await this.restore();
     }
     find = (search) => {
         return document.querySelector(search);
     }
     restore = async () => {
-        document.querySelectorAll(`section[data-page]`).forEach(section => {
-            let index = sessionStorage.getItem(section.dataset.page);
-            if (index) {
-                this.swipe(section.dataset.page, parseInt(index));
-            } else {
-                this.swipe(section.dataset.page, 1);
-            }
+
+        this.pages = JSON.parse(sessionStorage.getItem("_cache"));
+
+        this.pages.forEach(element => {
+            this.swipe(element.name, parseInt(element.index), element.description);
         });
+
     }
 }
 
@@ -58,7 +92,7 @@ class Alchemicats {
 document.addEventListener("DOMContentLoaded", async () => {
 
     const alchemicats = new Alchemicats();
-    await alchemicats.load();
-    await alchemicats.restore();
+    await alchemicats.init();
+   
 
 });
