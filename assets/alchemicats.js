b35c5eb356cdd3741c2c641aaac216f0f411329a
Dekram McKraken <mkythethinker@mky-soft.com>
2024-02-11 01:34:33 +0100

    scripts

--- assets/alchemicats.js
+++ assets/alchemicats.js
@@ -1,11 +1,23 @@
 
 class Alchemicats {
     constructor() {
-        
+        document.querySelectorAll('nav[data-page="swiper"]').forEach(swiper => {
+            swiper.addEventListener("click", async (evt) => {
+                evt.preventDefault();
+                this.swipe(evt.target.dataset.page, evt.target.dataset.pageIndex);
+            });
+        });
     }
 
-    swipe = (name, index) => {
-        document.querySelectorAll(`section[data-page=${name}]`).forEach(section => {
+    get = async (name) => {
+        const response = await fetch(`leaf/${name}.html`);
+        const page = await response.text();
+        return page;
+    }
+    swipe = async (name, index) => {
+        document.querySelectorAll(`section[data-page=${name}]`).forEach(async section => {
+
+            //const html = await this.get("test");
 
             if (parseInt(section.dataset.pageIndex) != index) {
                 section.classList.add("hidden");
@@ -13,30 +25,25 @@ class Alchemicats {
             else {
                 section.classList.remove("hidden");
             }
-            
             sessionStorage.setItem(name, index);
-            
-
-      
-
             document.querySelectorAll(`a[data-page="${name}"]`).forEach(nav => {
                 nav.classList.remove('active');
             });
-
             document.querySelectorAll(`a[data-page="${name}"][data-page-index="${index}"]`).forEach(nav => {
                 nav.classList.add('active');
             });
-
-            
-           
         });
     };
-
-    restore = ()=> {
-        
-
+    load = async () => {
+       
+        this.find("main header").innerHTML = await this.get("header");
+        this.find("#socialMedia").innerHTML = await this.get("socialMedia");
+    }
+    find = (search) => {
+        return document.querySelector(search);
+    }
+    restore = async () => {
         document.querySelectorAll(`section[data-page]`).forEach(section => {
-
             let index = sessionStorage.getItem(section.dataset.page);
             if (index) {
                 this.swipe(section.dataset.page, parseInt(index));
@@ -45,30 +52,13 @@ class Alchemicats {
             }
         });
     }
-
 }
 
-
-
-
+//Document ready
 document.addEventListener("DOMContentLoaded", async () => {
 
     const alchemicats = new Alchemicats();
-
-
-    document.querySelectorAll('nav[data-page="swiper"]').forEach(swiper => {
-        swiper.addEventListener("click", async (evt) => {
-            evt.preventDefault();
-           
-            let page = evt.target.dataset.page;
-            let index = evt.target.dataset.pageIndex;
-            alchemicats.swipe(page, index);
-
-            
-        });
-    });
-
-
-    alchemicats.restore();
+    await alchemicats.load();
+    await alchemicats.restore();
 
 });
