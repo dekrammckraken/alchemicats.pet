2907c5cdcffdc2a98f563d6bb004f65cf14c2c1e
Dekram McKraken <mkythethinker@mky-soft.com>
2024-08-08 15:49:44 +0200

    vars

--- assets/alchemy.js
+++ assets/alchemy.js
@@ -1,6 +1,17 @@
 const DOMContentLoaded = "DOMContentLoaded";
-const TIME_MINUTE = 6000;
-class Alchemy {1
+const TIME_MINUTE = 60000;
+
+const ANI_END = "animationend";
+const TOUCH_START = "touchstart";
+const TOUCH_MOVE = "touchmove";
+const TOUCH_END = "touchend";
+
+const MOUSE_LEAVE = "mouseleave";
+const MOUSE_MOVE = "mousemove";
+const EVT_CLICK = "click";
+
+class Alchemy {
+  1;
   constructor() {
     this.pages = [];
     this.touchStartX = 0;
@@ -8,145 +19,156 @@ class Alchemy {1
     this.touchStartY = 0;
   }
 
-  getVisitorsCount = async ()=> {
+  ghostsAppaerance = async () => {
     let response = await fetch(`/public/report.json`);
     const json = await response.json();
 
-    json.visitors.data.forEach((visitor)=> {
-      let counter = document.querySelector("span[data-visit=counter]")
-      return `${visitor.visitors.count} ghosts passed through here, so who brought the marshmallows?`;
+    for (const visitor of json.visitors.data) {
+      return visitor.visitors.count;
+    };
+  };
+  schedule = async (fun, runNow = true) => {
+    if (runNow)
+        fun();
+
+    setInterval(fun, TIME_MINUTE);
+  };
+  ui = async () => {
+    this.all(".swipeable").forEach(async (pane) => {
+      await this.UIEvents(pane);
+    });
+
+    this.schedule( async ()=> {
+
+        this.single("#highlitedItem").innerHTML = await this.ghostsAppaerance();
+
     });
   };
-  schedule = async (fun)=> {
-    setInterval(fun, TIME_MINUTE);
-  }
-  ui = () => {
-    document.querySelectorAll(".swipeable").forEach((pane) => {
-      pane.addEventListener(
-        "mouseleave",
-        async (evt) => {
-          let article = pane.closest("article");
-          article.classList.remove("right");
-          article.classList.remove("left");
-        },
-        { passive: true }
-      );
 
-      pane.addEventListener("mousemove", async (evt) => {
-        const clickX = evt.clientX - pane.getBoundingClientRect().left;
-        const paneWidth = pane.offsetWidth;
-        const paneCenterX = paneWidth / 2;
+  UIEvents = async (item) => {
 
-        let article = pane.closest("article");
-
+    item.addEventListener(
+      MOUSE_LEAVE,
+      async (evt) => {
+        let article = item.closest("article");
         article.classList.remove("right");
         article.classList.remove("left");
+      },
+      { passive: true }
+    );
+    item.addEventListener(MOUSE_MOVE, async (evt) => {
+      const clickX = evt.clientX - item.getBoundingClientRect().left;
+      const paneWidth = item.offsetWidth;
+      const paneCenterX = paneWidth / 2;
 
-        if (clickX > paneCenterX) {
-          article.classList.add("left");
-        } else {
-          article.classList.add("right");
-        }
-      });
+      let article = item.closest("article");
 
-      pane.addEventListener(
-        "click",
-        (evt) => {
-          const clickX = evt.clientX - pane.getBoundingClientRect().left;
-          const paneWidth = pane.offsetWidth;
-          const paneCenterX = paneWidth / 2;
-          let swipeable = evt.target.closest(".swipeable");
+      article.classList.remove("right");
+      article.classList.remove("left");
 
-          let index = parseInt(swipeable.dataset.pageIndex);
-          let next = clickX > paneCenterX;
-
-          this.swipe(
-            swipeable.dataset.page,
-            index,
-            swipeable.dataset.pageDescription,
-            next
-          );
-        },
-        { passive: true }
-      );
-
-      pane.addEventListener(
-        "touchstart",
-        async (evt) => {
-          this.touchStartX = evt.touches[0].clientX;
-          this.touchStartY = evt.touches[0].clientY;
-
-          evt.target.classList.add("swiping");
-        },
-        { passive: true }
-      );
-      document.addEventListener(
-        "touchmove",
-        async (evt) => {
-          let currentY = evt.touches[0].clientY;
-
-          if (currentY > this.touchStartY) evt.preventDefault();
-        },
-        { passive: true }
-      );
-      pane.addEventListener(
-        "touchend",
-        async (evt) => {
-          evt.target.classList.remove("swiping");
-          this.touchEndX = evt.changedTouches[0].clientX;
-          const SWIPE_THRESHOLD = 70;
-          let swipeable = evt.target.closest(".swipeable");
-          let index = parseInt(swipeable.dataset.pageIndex);
-
-          let swipelen = this.touchEndX - this.touchStartX;
-          let next = true;
-
-          if (Math.abs(swipelen) < SWIPE_THRESHOLD) return;
-
-          if (swipelen > 0 && Math.abs(swipelen) > SWIPE_THRESHOLD) {
-            next = false;
-          }
-
-          this.swipe(
-            swipeable.dataset.page,
-            index,
-            swipeable.dataset.pageDescription,
-            next
-          );
-        },
-        { passive: true }
-      );
+      if (clickX > paneCenterX) {
+        article.classList.add("left");
+      } else {
+        article.classList.add("right");
+      }
     });
-  };
-  defaultOrCachePages = async () => {
+    item.addEventListener(
+      EVT_CLICK,
+      (evt) => {
+        const clickX = evt.clientX - item.getBoundingClientRect().left;
+        const paneWidth = item.offsetWidth;
+        const paneCenterX = paneWidth / 2;
+        let swipeable = evt.target.closest(".swipeable");
+
+        let index = parseInt(swipeable.dataset.pageIndex);
+        let next = clickX > paneCenterX;
+
+        this.swipe(
+          swipeable.dataset.page,
+          index,
+          swipeable.dataset.pageDescription,
+          next
+        );
+      },
+      { passive: true }
+    );
+    item.addEventListener(
+      TOUCH_START,
+      async (evt) => {
+        this.touchStartX = evt.touches[0].clientX;
+        this.touchStartY = evt.touches[0].clientY;
+        evt.target.classList.add("swiping");
+      },
+      { passive: true }
+    );
+    document.addEventListener(
+      TOUCH_MOVE,
+      async (evt) => {
+        let currentY = evt.touches[0].clientY;
+
+        if (currentY > this.touchStartY) evt.preventDefault();
+      },
+      { passive: true }
+    );
+    item.addEventListener(
+      TOUCH_END,
+      async (evt) => {
+        evt.target.classList.remove("swiping");
+        this.touchEndX = evt.changedTouches[0].clientX;
+        const SWIPE_THRESHOLD = 70;
+        let swipeable = evt.target.closest(".swipeable");
+        let index = parseInt(swipeable.dataset.pageIndex);
+
+        let swipelen = this.touchEndX - this.touchStartX;
+        let next = true;
+
+        if (Math.abs(swipelen) < SWIPE_THRESHOLD) return;
+
+        if (swipelen > 0 && Math.abs(swipelen) > SWIPE_THRESHOLD) {
+          next = false;
+        }
+
+        this.swipe(
+          swipeable.dataset.page,
+          index,
+          swipeable.dataset.pageDescription,
+          next
+        );
+      },
+      { passive: true }
+    );
+  }
+  
+  defaultPages = async () => {
     let cache = null;
     if (cache == null) {
       this.pages = [];
-      document
-        .querySelectorAll(`article[data-page]`)
-        .forEach(async (section) => {
-          let page = {
-            name: section.dataset.page,
-            index: 0,
-            max: parseInt(section.dataset.pageMax),
-            description: section.dataset.pageDescription,
-          };
-          this.pages.currentPage = 1;
-          this.pages.push(page);
-        });
+      this.all(`article[data-page]`).forEach(async (section) => {
+        let page = {
+          name: section.dataset.page,
+          index: 0,
+          max: parseInt(section.dataset.pageMax),
+          description: section.dataset.pageDescription,
+        };
+        this.pages.currentPage = 1;
+        this.pages.push(page);
+      });
     } else {
       this.pages = JSON.parse(cache);
     }
     sessionStorage.setItem("_cache", JSON.stringify(this.pages));
   };
-  get = async (name) => {
+  
+  getFlake = async (name) => {
     const response = await fetch(`leaf/${name}.html`);
     const page = await response.text();
     return page;
   };
+  
   placeHolder = async (name, val, str) => {
     return str.replace(name, val);
   };
+  
   swipe = async (name, index, description, next) => {
     let currentPage = this.pages.find((page) => {
       return page.name == name;
@@ -154,18 +176,17 @@ class Alchemy {1
 
     next ? index++ : index--;
 
-    let pane = document.querySelectorAll(
+    let pane = this.single(
       `section[data-page="${name}"][data-page-index="${index}"]`
-    )[0];
+    );
 
-    if (pane === undefined)
-      return;
+    if (pane === undefined || pane == null) return;
 
     if (next) pane.classList.add("page-flip");
     else pane.classList.add("page-flip-reverse");
 
     pane.addEventListener(
-      "animationend",
+      ANI_END,
       (evt) => {
         evt.target.classList.remove("page-flip");
         evt.target.classList.remove("page-flip-reverse");
@@ -175,40 +196,34 @@ class Alchemy {1
 
     if (currentPage) currentPage.index = index;
 
-    document
-      .querySelectorAll(`section[data-page=${name}]`)
-      .forEach(async (section) => {
-        if (parseInt(section.dataset.pageIndex) != index) {
-          section.classList.add("hidden");
-        } else {
-          section.classList.remove("hidden");
-        }
+    this.all(`section[data-page=${name}]`).forEach(async (section) => {
+      if (parseInt(section.dataset.pageIndex) != index) {
+        section.classList.add("hidden");
+      } else {
+        section.classList.remove("hidden");
+      }
 
-        document
-          .querySelectorAll(`span[data-page="${name}"]`)
-          .forEach((nav) => {
-            nav.classList.remove("active");
-          });
-        document
-          .querySelectorAll(
-            `span[data-page="${name}"][data-page-index="${index}"]`
-          )
-          .forEach((nav) => {
-            nav.classList.add("active");
-          });
-        document.querySelector(
-          `article[data-page="${name}"] > h2`
-        ).innerHTML = `${currentPage.description}`;
+      this.all(`span[data-page="${name}"]`).forEach((nav) => {
+        nav.classList.remove("active");
       });
+      this.all(`span[data-page="${name}"][data-page-index="${index}"]`).forEach(
+        (nav) => {
+          nav.classList.add("active");
+        }
+      );
+      this.single(
+        `article[data-page="${name}"] > h2`
+      ).innerHTML = `${currentPage.description}`;
+    });
 
     sessionStorage.setItem("_cache", JSON.stringify(this.pages));
   };
   init = async () => {
-    this.find("main header").innerHTML = await this.get("header");
-    this.find("#socialMedia").innerHTML = await this.get("socialMedia");
-    this.pages = await this.defaultOrCachePages();
+    this.single("main header").innerHTML = await this.getFlake("header");
+    this.single("#socialMedia").innerHTML = await this.getFlake("socialMedia");
+    this.pages = await this.defaultPages();
     await this.restore();
-    this.ui();
+    await this.ui();
   };
   bday = () => {
     let d2 = new Date(new Date().getFullYear(), 3, 5);
@@ -222,9 +237,14 @@ class Alchemy {1
       d: days,
     };
   };
-  find = (search) => {
+
+  single = (search) => {
     return document.querySelector(search);
   };
+  all = (search) => {
+    return document.querySelectorAll(search);
+  };
+
   restore = async () => {
     this.pages = JSON.parse(sessionStorage.getItem("_cache"));
     this.pages.forEach((element) => {
